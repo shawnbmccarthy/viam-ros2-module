62042518cf39c00020ea1d4facba18c0c11093e0
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2023-08-11 17:08:55 +0200

    tested should work now

--- setup.sh
+++ setup.sh
@@ -9,6 +9,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 #sudo apt install python3-pip
 #sudo apt-get install python3-venv
 
-#python3 -m venv ${SCRIPT_DIR}/venv
+python3 -m venv ${SCRIPT_DIR}/venv
 
 exec ${SCRIPT_DIR}/venv/bin/python -m pip install -r ${SCRIPT_DIR}/requirements.txt
