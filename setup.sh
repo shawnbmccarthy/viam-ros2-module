2296d7caed811981fe85a51c5954c8456363a34a
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2023-08-11 15:57:31 +0200

    added venv setup script

--- setup.sh
+++ setup.sh
@@ -3,3 +3,12 @@
 # and add any environment variables needed to our run.sh
 # script
 #
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+#sudo apt install python3-pip
+#sudo apt-get install python3-venv
+
+#python3 -m venv ${SCRIPT_DIR}/venv
+
+exec ${SCRIPT_DIR}/venv/bin/python -m pip install -r ${SCRIPT_DIR}/requirements.txt
