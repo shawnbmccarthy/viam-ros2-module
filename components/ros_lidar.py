cdca63c7cdf264eaa0ac342779590b490d834ed7
shawnbmccarthy <sbmccarth@gmail.com>
2023-08-13 22:34:11 -0400

    update lidar

--- components/ros_lidar.py
+++ components/ros_lidar.py
@@ -1,9 +1,11 @@
 import logging
+import math
 import numpy as np
 import rclpy
 import viam
 
 from rclpy.node import Node
+from rclpy.subscription import Subscription
 from PIL.Image import Image
 from sensor_msgs.msg import LaserScan
 from threading import Lock
@@ -11,6 +13,7 @@ from typing import ClassVar, List, Mapping, Optional, Sequence, Tuple, Union
 from typing_extensions import Self
 from utils import RclpyNodeManager
 from viam.components.camera import Camera, DistortionParameters, IntrinsicParameters, RawImage
+from viam.components.movement_sensor import Vector3
 from viam.logging import getLogger
 from viam.media.video import NamedImage 
 from viam.module.types import Reconfigurable
@@ -19,44 +22,18 @@ from viam.proto.common import ResourceName, ResponseMetadata
 from viam.resource.base import ResourceBase
 from viam.resource.registry import Registry, ResourceCreatorRegistration
 from viam.resource.types import Model, ModelFamily
-
-
-class RosLidarNode(Node):
-    # TODO: enable_rosout should be arg
-    def __init__(self, lidar_topic, node_name):
-        super().__init__(node_name, enable_rosout=True)
-        self.lock = Lock()
-        self.msg = None
-        qos_policy = rclpy.qos.QoSProfile(
-            reliability=rclpy.qos.ReliabilityPolicy.BEST_EFFORT,
-            history=rclpy.qos.HistoryPolicy.KEEP_LAST,
-            depth=1
-        )
-
-        self.subscriber = self.create_subscription(LaserScan, lidar_topic, self.subscriber_callback, qos_profile=qos_policy)
-
-    def subscriber_callback(self, msg):
-        with self.lock:
-            self.get_logger().info(f'msg -> {msg}')
-            self.msg = msg
-
-
-class RosLidarProperties(Camera.Properties):
-    def __init__(self):
-        super().__init__(
-            supports_pcd=True,
-            intrinsic_parameters=IntrinsicParameters(width_px=0, height_px=0, focal_x_px=0.0, focal_y_px=0.0, center_x_px=0.0),
-            distortion_parameters=DistortionParameters(model='')
-        )
+from .viam_ros_node import ViamRosNode
 
 
 class RosLidar(Camera, Reconfigurable):
     MODEL: ClassVar[Model] = Model(ModelFamily('viamlabs', 'ros2'), 'lidar')
     logger: logging.Logger
-    node_name: str
     ros_topic: str
     ros_node: Node
+    subscription: Subscription
     ros_lidar_properties: Camera.Properties
+    lock: Lock
+    msg: LaserScan
 
     @classmethod
     def new(cls, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]) -> Self:
@@ -75,21 +52,36 @@ class RosLidar(Camera, Reconfigurable):
 
     def reconfigure(self, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]):
         self.ros_topic = config.attributes.fields['ros_topic'].string_value
-        self.node_name = config.attributes.fields['ros_node_name'].string_value
         self.ros_lidar_properties = Camera.Properties(
             supports_pcd=True,
             intrinsic_parameters=IntrinsicParameters(width_px=0, height_px=0, focal_x_px=0.0, focal_y_px=0.0, center_x_px=0.0),
             distortion_parameters=DistortionParameters(model='')
         )
-        if self.node_name == '' or self.node_name == None:
-            self.node_name = 'VIAM_ROS_LIDAR_NODE'
         
-        rcl_mgr = RclpyNodeManager.get_instance()
-        if self.ros_node != None:
-            rcl_mgr.remove_node(self.ros_node)
+        if self.ros_node is not None:
+            if self.subscription is not None:
+                self.ros_node.destroy_subscription(self.subscription)
+        else:
+            self.ros_node = ViamRosNode()
 
-        self.ros_node = RosLidarNode(self.ros_topic, self.node_name)
-        rcl_mgr.spin_and_add_node(self.ros_node)
+        qos_policy = rclpy.qos.QoSProfile(
+            reliability=rclpy.qos.ReliabilityPolicy.BEST_EFFORT,
+            history=rclpy.qos.HistoryPolicy.KEEP_LAST,
+            depth=1
+        )
+
+        self.subscription = self.ros_node.create_subscription(
+            Imu,
+            self.ros_topic,
+            self.subscriber_callback,
+            qos_profile=qos_policy
+        )
+        self.lock = Lock()
+        self.msg = None
+
+    def subscriber_callback(self, msg):
+        with self.lock:
+            self.msg = msg
 
     async def get_image(self, mime_type: str='', *, timeout: Optional[float]=None, **kwargs) -> Union[Image, RawImage]:
         raise NotImplementedError()
@@ -98,9 +90,17 @@ class RosLidar(Camera, Reconfigurable):
         raise NotImplementedError()
 
     async def get_point_cloud(self, *, timeout: Optional[float]=None, **kwargs) -> Tuple[bytes, str]:
-        if self.ros_node.msg == None:
+        if self.msg is None:
             raise Exception('laserscan msg not ready')
-        self.logger.info(f'lidar msg: {self.ros_node.msg}')
+        #
+        #for idx, range in enumerate(msg.ranges):
+        #    if range < msg.range_min or r > msg.range_max:
+        #        continue
+        #    v = Vector3()
+        #    ang = msg.angle_min * (i * msg.angle_increment)
+        #    v.y = 1000 * math.sin(ang) * range
+        #    v.x = 1000 * math.cos(ang) * range
+        self.logger.info(msg)
         raise NotImplementedError()
 
     async def get_properties(self, *, timeout: Optional[float] = None, **kwargs) -> Camera.Properties:
